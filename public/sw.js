8d6b0ee463b79371c064ab07de656b2bb228f224
Sam Wood <16246185+sw517@users.noreply.github.com>
2023-12-07 16:57:35 +0000

    WIP PWA

--- public/sw.js
+++ public/sw.js
@@ -1,14 +1,46 @@
-const staticBillSplitter = "bill-splitter-v1"
-const assets = [
-  "/",
-  "/index.html",
-  "/css/style.css",
-]
+// This is the "Offline page" service worker
 
-self.addEventListener("install", installEvent => {
-  installEvent.waitUntil(
-    caches.open(staticBillSplitter).then(cache => {
-      cache.addAll(assets)
-    })
-  )
-})
+importScripts('https://storage.googleapis.com/workbox-cdn/releases/5.1.2/workbox-sw.js');
+
+const CACHE = "bill-splitter-pwa";
+
+const offlineFallbackPage = "index.html";
+
+self.addEventListener("message", (event) => {
+  if (event.data && event.data.type === "SKIP_WAITING") {
+    self.skipWaiting();
+  }
+});
+
+self.addEventListener('install', async (event) => {
+  event.waitUntil(
+    caches.open(CACHE)
+      .then((cache) => cache.add(offlineFallbackPage))
+  );
+});
+
+if (workbox.navigationPreload.isSupported()) {
+  workbox.navigationPreload.enable();
+}
+
+self.addEventListener('fetch', (event) => {
+  if (event.request.mode === 'navigate') {
+    event.respondWith((async () => {
+      try {
+        const preloadResp = await event.preloadResponse;
+
+        if (preloadResp) {
+          return preloadResp;
+        }
+
+        const networkResp = await fetch(event.request);
+        return networkResp;
+      } catch (error) {
+
+        const cache = await caches.open(CACHE);
+        const cachedResp = await cache.match(offlineFallbackPage);
+        return cachedResp;
+      }
+    })());
+  }
+});
