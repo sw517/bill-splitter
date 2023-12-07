e6785ba927f5a0bdbf8c6a57aafb177751bca3f2
Sam Wood <16246185+sw517@users.noreply.github.com>
2023-12-07 16:22:51 +0000

    WIP PWA

--- sw.js
+++ sw.js
@@ -2,7 +2,18 @@
 
 importScripts('https://storage.googleapis.com/workbox-cdn/releases/5.1.2/workbox-sw.js');
 
-const CACHE = "pwabuilder-page";
+const CACHE = "bill-splitter-pwa";
+
+window.addEventListener('beforeinstallprompt', (e) => {
+  // Prevent the mini-infobar from appearing on mobile
+  e.preventDefault();
+  // Stash the event so it can be triggered later.
+  deferredPrompt = e;
+  // Update UI notify the user they can install the PWA
+  showInstallPromotion();
+  // Optionally, send analytics event that PWA install promo was shown.
+  console.log(`'beforeinstallprompt' event was fired.`);
+});
 
 const offlineFallbackPage = "index.html";
 
